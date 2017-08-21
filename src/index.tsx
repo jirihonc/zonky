9a0f22391e01412b7bf2da28d4120828bff95198
jhonc <jiri.honc@mgm-tp.com>
2017-08-21 16:48:55 +0200

    app update

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import RatingForm from "./components/RatingForm";
 
 
 ReactDOM.render(
-    <RatingForm label="Zonky, vypocet prumerne pujcky v zavislosti na ratingu" />, 
+    <RatingForm label="Zonky, výpočet průměrné půjčky v závislosti na ratingu" />, 
     document.getElementById("content")
 );
